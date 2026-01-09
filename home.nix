e55833df09de0139e9b833182a5602d392154717
Lemuel Guevara <lemuelguevara@gmail.com>
2026-01-09 18:20:23 +0800

    feat(home.nix): added cava

--- home.nix
+++ home.nix
@@ -51,6 +51,13 @@ in {
         type "pipewire"
         name "PipeWire Sound Server"
       }
+
+      audio_output {
+        type                    "fifo"
+        name                    "Visualizer feed"
+        path                    "/tmp/mpd.fifo"
+        format                  "44100:16:2"
+      }
     '';
   };
 
@@ -106,6 +113,7 @@ in {
     nvitop
     pfetch
     rmpc
+    cava
 
     # Archives & Files
     zip
